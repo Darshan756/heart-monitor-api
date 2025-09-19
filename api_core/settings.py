720259456b54c11ec29ffd0678b028f9b90db0c1
Darshan MT <darshanmalli93@gmail.com>
2025-09-19 17:42:47 +0000

    last commit

--- api_core/settings.py
+++ api_core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y054!i52r-8ze%$6u9xgju#9@!x^)e42$ve_l-z^m)25^=f)-x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -152,11 +153,11 @@ REST_FRAMEWORK = {
 
 AUTH_USER_MODEL = 'user_account.CustomUser'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'darshanmalli93@gmail.com'
-EMAIL_HOST_PASSWORD = 'fbcr gdhk jkxy vhzt'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
